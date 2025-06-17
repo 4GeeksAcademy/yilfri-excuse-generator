4d8659cd38f295d50d1094e9d171d86815faa42d
Yilfri Salave <yilfri97@hotmail.com>
2025-06-17 00:16:22 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function random(options) {
+    let randomOption = Math.floor(Math.random() * options.length)
+    return options[randomOption] 
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.getElementById("excuse")
+  excuse.innerHTML= `<h2>${random(who)} ${random(action)} ${random(what)} ${random(when)}</h2>`;
 };
